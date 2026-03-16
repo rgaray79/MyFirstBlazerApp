85b1a7e2fa9600a4d2b21b5b9258efb7d11c9602
Ricardo Garay <rgarayquiroz@gmail.com>
2026-03-16 14:44:27 -0400

    03-16 end day

---
+++ wwwroot/Services/ApiService.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Configuration;
+
+public class ApiService
+{
+    private readonly IConfiguration _configuration;
+
+    public ApiService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GetApiUrl()
+    {
+        return _configuration["ApiSettings:BaseUrl"];
+    }
+}
